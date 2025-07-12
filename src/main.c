16bfe84143270bd973d511efeda07c39a3a03b1c
Felix Alvo <felix.alvo@outlook.com>
2025-07-12 14:36:53 +0200

    added .vscode to .gitignore and tested utils.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include "neural_net.h"
+#include "utils.h"
 
 int main(){
     hello_net();
+    utils_check();
     return 0;
 }
---
+++ src/utils.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "utils.h"
+
+void utils_check(){
+    printf("util file works\n");
+}
